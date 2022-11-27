53d091eaed85229cd05f35b5a31be805171e5157
dimachilaia <chilaiadima@gmail.com>
2022-11-27 21:02:36 +0400

    finished

--- secondhomework/index.js
+++ secondhomework/index.js
@@ -1,4 +1,4 @@
-// 11
+11
 const array = [
   {
     name: "Temo",
@@ -15,32 +15,45 @@ const array = [
 ];
 
 const FirstFunc = (param)=>{
-  const newArray = param.filter((item)=>item.age < 22).map((item)=>item.name)
-  console.log(newArray)
+ const newName = param.reduce((acc, curr)=>curr.age < acc.age ? curr.name :acc)
+ console.log(newName)
 }
-FirstFunc(array)
+FirstFunc(array) 
+
+
+//2
+const info = {
+  name:'Dima',
+  lastName:'Tchilaia',
+  age:25,
+  about:'React Developer'
+}
+const secondInfo = {
+  ...info
+}
+console.log(secondInfo)
+
 
 // 3
+const ThirdFunction = () => {
+  while (true) {
+    const FirstPlayerNumb = Math.trunc(Math.random() * 6) + 1;
+    const SecondPlayerNumb = Math.trunc(Math.random() * 6) + 1;
+    console.log(
+      "First Player Number is" + ` ${FirstPlayerNumb}`,
+      "And Second Player Number is" + ` ${ SecondPlayerNumb}`
+    );
+    if (FirstPlayerNumb === 3) {
+      console.log("A is Winner!");
+      console.log(FirstPlayerNumb);
+    } else if (SecondPlayerNumb === 3) {
+      console.log("B is winner!");
+      console.log(SecondPlayerNumb);
+    } else {
+      console.log("Draw!");
+    }
+    break;
+  }
+};
 
-// const ThirdFunction = () => {
-//   while (true) {
-//     const FirstPlayerNumb = Math.trunc(Math.random() * 6) + 1;
-//     const SecondPlayerNumb = Math.trunc(Math.random() * 6) + 1;
-//     console.log(
-//       "First Player Number is" + ` ${FirstPlayerNumb}`,
-//       "And Second Player Number is" + ` ${ SecondPlayerNumb}`
-//     );
-//     if (FirstPlayerNumb === 3) {
-//       console.log("A is Winner!");
-//       console.log(FirstPlayerNumb);
-//     } else if (SecondPlayerNumb === 3) {
-//       console.log("B is winner!");
-//       console.log(SecondPlayerNumb);
-//     } else {
-//       console.log("Draw!");
-//     }
-//     break;
-//   }
-// };
-
-// ThirdFunction();
+ThirdFunction();
